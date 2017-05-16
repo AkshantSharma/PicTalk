f8f635bdf3714b7a3c194c95804348e65bd4c876
Ashiwn Palaparthi <ashwin.palaparthi@gmail.com>
2017-05-17 01:29:06 +0530

    new
    new

---
+++ Droid/Activities/SplashScreen.cs
@@ -0,0 +1,30 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace PicTalk.Droid.Activities
+{
+	[Activity(Label = "Splash Screen App", MainLauncher = true, Theme = "@style/Theme.Splash", NoHistory = true, Icon = "@mipmap/icon")]
+    public class SplashScreen : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your application here
+            //Display Splash Screen for 4 Sec  
+            Thread.Sleep(4000);
+            //Start Activity1 Activity  
+            StartActivity(typeof(DashboardActivity));
+        }
+    }
+}

a177f10eb15db63aa2b43d71b029161d4c27d239
Ashiwn Palaparthi <ashwin.palaparthi@gmail.com>
2017-05-20 18:02:27 +0530

    deleted

--- PicTalk/Helpers/PTConstant.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace PicTalk.Helpers
-{
-    public static class PTConstant
-    {
-        public static string Host = "https://api.fantasydata.net/v3/nfl/scores/json";//Base Url of server
-        public static string ErrorApi = "";//Error Url of server
-    }
-}
